5e445bbdd0c0872e2f2b15b28d4efadadcd9c00a
Anurag <sankhaanurag2005@gmail.com>
2026-07-05 14:34:19 +0530

    user registration done

--- Backend/app.js
+++ Backend/app.js
@@ -4,11 +4,21 @@ dotenv.config();
 const express = require("express")
 const app = express()
 
+const connectToDb = require("./db/db.js")
+connectToDb();
+
 const cors = require('cors')
 app.use(cors())
 
-app.get('/',(req,res)=>{
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const userRoutes = require('./routes/user.routes');
+
+app.get('/', (req, res) => {
     res.send("Hello World")
 })
 
-module.exports=app;
+app.use('/users', userRoutes);
+
+module.exports = app;
---
+++ Backend/models/user.model.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        firstname: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength:[3,'First name must be at least 3 characters long']
+        },
+        lastname:{
+            type:String,
+            required:true,
+            trim:true,
+            minlength:[3,'Last name must be at least 3 characters long']
+        }
+
+    },
+    email:{
+        type:String,
+        required:true,
+        trim:true,
+        unique:true,
+        lowercase:true,
+        match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Please provide a valid email address"]
+    },
+    password:{
+        type:String,
+        required:true,
+        trim:true,
+        select:false,
+        minlength:[6,'Password must be at least 6 characters long']
+    },
+    socketId:{
+        type:String,
+        default:""
+    },
+    location:{
+        
+    }
+})
+
+userSchema.methods.generateAuthToken = function(){
+    const token = jwt.sign({_id:this._id},process.env.JWT_SECRET);
+    return token;
+}
+
+userSchema.methods.comparePassword = async function(password){
+    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.statics.hashedPassword = async function (password){
+    return await bcrypt.hash(password,10);
+}
+
+const userModel = mongoose.model('user', userSchema);
+
+module.exports = userModel;
